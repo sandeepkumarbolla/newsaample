2fedd5b29a42f58bcffb3dcfb848efc1872eda3e
sandeep kumar bolla <sandeepchowdarybolla159@gmail.com>
2022-06-01 22:59:20 +0530

    new version added

--- src/main/java/io/amigos/studentSample/StudentSampleApplication.java
+++ src/main/java/io/amigos/studentSample/StudentSampleApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentSampleApplication {
+	public String hello(){
+		return "hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentSampleApplication.class, args);
